7d262ac6d70808e9dfcaadeba34758bdfc429972
salmigl <85357848+salmigl@users.noreply.github.com>
2022-11-17 09:05:46 -0300

    Add files via upload

---
+++ src/components/Button/QuestionCard/index.js
@@ -0,0 +1,23 @@
+import "./index.css";
+function QuestionCard({ preguntaActual }) {
+    return (
+        <div className="box">
+            <div className="mi-clase">
+                <span className="tag is-rounded is-info" > {preguntaActual.id}</span>
+                <span><strong> {preguntaActual.question} </strong></span>
+                <br />
+            </div>
+            {
+
+                preguntaActual.answers.map((opcion) => (
+                    <div key={opcion.id} > <input type="radio" id={`${opcion.id}`} name={opcion.id} value={opcion.answer} >  </input>
+                        <label htmlFor={`${opcion.id}`}> {opcion.answer}</label> 
+                    </div>
+                ))
+            }
+            <br />
+        </div>
+
+    )
+}
+export default QuestionCard;
---
+++ src/pages/game/index.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+import QuestionCard from "../../components/Button/QuestionCard";
+
+const API_URL = " https://62bb6e36573ca8f83298fbef.mockapi.io/metcampweb22/v1/questions/harry-potter";
+
+function Game() {
+    const [loading, setLoading] = useState(true);
+    const [questions, setQuestions] = useState([]);
+    console.log(questions);
+    useEffect(() => {
+        fetch(API_URL)
+            .then(respose => respose.json())
+            .then(data => setQuestions(data))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [])
+    return (
+        <div className="container">
+            <section className="section">
+                <nav class="breadcrumb" aria-label="breadcrumbs">
+                    <ul>
+                        <li>
+                            <Link to="/">home</Link>
+                        </li>
+                        <li class="is-active">
+                            <Link to="/game">Juego</Link>
+                        </li>
+                    </ul>
+                </nav>
+                {
+                    loading && (
+                        <div> Cargando...</div>
+                    )
+                }
+                {
+                    !loading && (
+                        <form>
+                            {
+                                questions.map((question) => {
+                                    return < QuestionCard preguntaActual={question} />
+                                })
+                            }
+                        </form>
+                    )
+                }
+                <h1>El Juego</h1>
+            </section>
+        </div>
+    );
+}
+
+export default Game;
